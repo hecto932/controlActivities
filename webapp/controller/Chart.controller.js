d250be5626f3b0ed9dc7cdcf3494055fbbfa0741
Hector Flores <hecto932@gmail.com>
2016-06-09 10:31:25 -0400

    Agregando grafica

--- webapp/controller/Chart.controller.js
+++ webapp/controller/Chart.controller.js
@@ -1,16 +1,68 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller"
 ], function(Controller) {
-	"use strict";
+    "use strict";
 
-	return Controller.extend("controlActivities.controller.Chart", {
-		onInit: function() {
-			var sPath = jQuery.sap.getModulePath("controlActivities.model", "/chart.json");
-			var oModel = new sap.ui.model.json.JSONModel(sPath);
-			console.log(oModel);
-			//var oVizFrame = this.getView().byId("idoVizFrame");
-			//oVizFrame.setModel(oModel);	
-		}
-	});
+    return Controller.extend("controlActivities.controller.Chart", {
+        onInit: function() {
+
+            var sPath = jQuery.sap.getModulePath("controlActivities.model", "/chart.json");
+            var oModel = new sap.ui.model.json.JSONModel(sPath);
+            console.log(oModel);
+
+            var oVizFrame = sap.ui.getCore().byId("oVizFrame");
+
+            oVizFrame.setModel(oModel);
+
+            var oDataset_bl = new sap.viz.ui5.data.FlattenedDataset({
+                dimensions: [{
+                    name: 'Dia',
+                    value: "{Dia}"
+                }],
+                measures: [{
+                    group: 1,
+                    name: 'Teorico',
+                    value: '{Teorico}'
+                }, {
+                    group: 1,
+                    name: 'Peso',
+                    value: '{Peso}'
+                }],
+                data: {
+                    path: "/Datos"
+                }
+            });
+
+            oVizFrame.setVizProperties({
+                plotArea: {
+                    showGap: true
+                }
+            });
+
+            oVizFrame.setDataset(oDataset_bl);
+
+            var feedPrimaryValues_bl7 = new sap.viz.ui5.controls.common.feeds.FeedItem({
+                    'uid': "primaryValues",
+                    'type': "Measure",
+                    'values': ["Teorico", "Peso"]
+                        //'values' : [ "Revenue" ]
+                }),
+                feedAxisLabels_bl7 = new sap.viz.ui5.controls.common.feeds.FeedItem({
+                    'uid': "axisLabels",
+                    'type': "Dimension",
+                    'values': ["Dia"]
+                }),
+                feedTargetValues_bl7 = new sap.viz.ui5.controls.common.feeds.FeedItem({
+                    'uid': "targetValues",
+                    'type': "Measure",
+                    'values': ["Peso"]
+                });
+
+            oVizFrame.addFeed(feedPrimaryValues_bl7);
+            oVizFrame.addFeed(feedAxisLabels_bl7);
+            oVizFrame.addFeed(feedTargetValues_bl7);
+            oVizFrame.setVizType('line');
+        }
+    });
 
 });
--- webapp/view/Chart.view.js
+++ webapp/view/Chart.view.js
@@ -14,39 +14,32 @@ sap.ui.jsview("controlActivities.view.Chart", {
 	 */
 	createContent: function(oController) {
 
-		jQuery.sap.require("sap.viz.ui5");
-
-		var oVizFrame = new sap.viz.ui5.controls.VizFrame("vizFrame", {
+		var oVizFrame = new sap.viz.ui5.controls.VizFrame("oVizFrame", {
 			height: "700px",
 			width: "100%",
-			uiConfig: {applicationSet : 'Fiori'}
+			uiConfig: { 
+				applicationSet: "fiori"
+			}
 		});
 
 		var oChartContainerContent = new sap.suite.ui.commons.ChartContainerContent({
-			icon: "sap-icon://bubble-chart",
-			title: "Bubble Chart",
-			content: [
-				oVizFrame
-			]
+			icon : "sap-icon://line-chart", 
+			title : "Line Chart",
+			content: oVizFrame
 		});
 
-		var oChartContainer = new sap.suite.ui.commons.ChartContainer("chartContainer", {
-			autoAdjustHeight: true,
-			contentChange: "attachContentChange",
-			personalizationPress: "attachPersonalizationPress",
-			showFullScreen: true,
-			showLegend: false,
-			title: "Halo",
-			content: [
-				oChartContainerContent
-			]	
+		var oChartContainer = new sap.suite.ui.commons.ChartContainer("oChartContainer", {
+			title: "Revenue",
+			content: oChartContainerContent
 		});
 
 		return new sap.m.Page({
 			title: "Grafica",
 			content: [
 				oChartContainer
-			]
+			],
+			enableScrolling : true,
+			showNavButton : true
 		});
 	}
 
