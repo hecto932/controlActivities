59567804f3c01c4ee6b198ebfa70c47ac8b3eb19
Hector Flores <hecto932@gmail.com>
2016-06-30 11:43:31 -0400

    Invalid hashes fixed

--- webapp/controller/NotFound.controller.js
+++ webapp/controller/NotFound.controller.js
@@ -1,10 +1,24 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/routing/History"
+], function (Controller, History) {
 	"use strict";
 	return Controller.extend("controlActivities.controller.NotFound", {
 		onInit: function () {
 
+		},
+		getRouter : function () {
+			return sap.ui.core.UIComponent.getRouterFor(this);
+		},
+		onNavBack: function (oEvent) {
+			var oHistory, sPreviousHash;
+			oHistory = History.getInstance();
+			sPreviousHash = oHistory.getPreviousHash();
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				this.getRouter().navTo("home", {}, true /*no history*/);
+			}
 		}
 	});
 });
